a7210479ed8ab87a04b6b8cde63dba2517c1a25d
Damrongwit Nusuk <jack@racksync.com>
2025-03-04 20:55:11 +0700

    feat: update Django and Gunicorn versions, restructure project files, and add WSGI/ASGI configurations

--- hands-on/django-basic/app/views.py
+++ hands-on/django-basic/app/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import platform
 import os
 
 
 def index(request):
     context = {
-        'title': 'DevOps Workshop - Django Example',
-        'python_version': platform.python_version(),
-        'django_version': os.environ.get('DJANGO_VERSION', 'Development'),
-        'hostname': platform.node(),
+        "title": "DevOps Workshop - Django Example",
+        "python_version": platform.python_version(),
+        "django_version": os.environ.get("DJANGO_VERSION", "Development"),
+        "hostname": platform.node(),
     }
-    return render(request, 'app/index.html', context)
+    return render(request, "app/index.html", context)
